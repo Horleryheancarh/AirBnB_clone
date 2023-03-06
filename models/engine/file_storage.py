c3d7b5402f1f5620dcfff9a41704c59a28b1431c
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-03-06 19:00:12 +0000

    created command line console

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class that saves instances to a JSON file
+and reads JSON file to instances
+"""
+import json
+import os
+
+
+class FileStrorage:
+    """
+    Class that reads and saves JSON objects
+    """
+    pass
